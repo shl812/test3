68a0390fe360e31b36e52b46c20fb96e5092d1b0
shl812 <46167159+shl812@users.noreply.github.com>
2022-12-21 21:18:36 +0900

    Add files via upload

---
+++ 32193119_mycp.c
@@ -0,0 +1,26 @@
+/**
+* file_test mycat.c: read data a file
+* @author : lee seo bin
+* @email:sltenic@naver.com
+* @version:
+* @ date: 2022 11 04
+**/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    FILE* fp = fopen(argv[1], "r");
+    FILE* nfp = fopen(argv[2], "w");
+    if (fp == NULL || nfp == NULL) {
+        perror("fopen");
+        return -1;
+    }
+    char buf[4096];
+    int ret = 0;
+    while (ret = fread(buf, sizeof(char), sizeof(buf), fp))
+        fwrite(buf, 1, ret, nfp);
+    fclose(fp);
+    fclose(nfp);
+    return 0;
+}
